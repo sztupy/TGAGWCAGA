294ba006a8687aa142cfea1d1b73196627001dc9
Zsolt Sz. Sztupák <mail@sztupy.hu>
2024-11-30 01:27:27 +0000

    Add support for poptrox popups for trailers and screenshots

--- assets/js/main.js
+++ assets/js/main.js
@@ -214,4 +214,73 @@
 
 		});
 
+		var $main = $('#page-wrapper');
+
+		// Thumbs.
+			$main.children('.thumb').each(function() {
+
+				var	$this = $(this),
+					$image = $this.find('.image'), $image_img = $image.children('img'),
+					x;
+
+				// No image? Bail.
+					if ($image.length == 0)
+						return;
+
+				// Image.
+				// This sets the background of the "image" <span> to the image pointed to by its child
+				// <img> (which is then hidden). Gives us way more flexibility.
+
+					// Set background.
+						$image.css('background-image', 'url(' + $image_img.attr('src') + ')');
+
+					// Set background position.
+						if (x = $image_img.data('position'))
+							$image.css('background-position', x);
+
+					// Hide original img.
+						$image_img.hide();
+
+			});
+
+		$main.poptrox({
+			baseZIndex: 20000,
+			caption: function($a) {
+
+				var s = '';
+
+				$a.nextAll().each(function() {
+					s += this.outerHTML;
+				});
+
+				return s;
+
+			},
+			fadeSpeed: 300,
+			onPopupClose: function() { $body.removeClass('modal-active'); },
+			onPopupOpen: function() { $body.addClass('modal-active'); },
+			overlayOpacity: 0,
+			popupCloserText: '',
+			popupHeight: 150,
+			popupLoaderText: '',
+			popupSpeed: 300,
+			popupWidth: 150,
+			selector: '.thumb > a.image',
+			usePopupCaption: true,
+			usePopupCloser: true,
+			usePopupDefaultStyling: false,
+			usePopupForceClose: true,
+			usePopupLoader: true,
+			usePopupNav: true,
+			windowMargin: 50
+		});
+
+		// Hack: Set margins to 0 when 'xsmall' activates.
+			breakpoints.on('<=xsmall', function() {
+				$main[0]._poptrox.windowMargin = 0;
+			});
+
+			breakpoints.on('>xsmall', function() {
+				$main[0]._poptrox.windowMargin = 50;
+			});
 })(jQuery);
